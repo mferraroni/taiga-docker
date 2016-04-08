41a55b314bc444266d55583f1fdcc128d9d9493d
Matteo Ferraroni <matteo.ferraroni@gmail.com>
2016-04-08 20:58:01 +0000

    Install gogs integration

--- backend/plugin-install.sh
+++ backend/plugin-install.sh
@@ -4,4 +4,3 @@ echo 'Installing gogs plugin'
 pip install taiga-contrib-gogs
 echo 'INSTALLED_APPS += ["taiga_contrib_gogs"]' >> /usr/src/app/taiga-back/settings/local.py
 echo 'PROJECT_MODULES_CONFIGURATORS["gogs"] = "taiga_contrib_gogs.services.get_or_generate_config";' >> /usr/src/app/taiga-back/settings/local.py
-python manage.py migrate taiga_contrib_gogs
